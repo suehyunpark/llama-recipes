d4b301d5ffbadef91f5b0ae6f07a7b5dc78e1a4c
suehyunpark <shparksue@gmail.com>
2024-10-24 15:58:10 +0900

    Update custom dataset script

--- recipes/quickstart/finetuning/datasets/arc_chat_dataset.py
+++ recipes/quickstart/finetuning/datasets/arc_chat_dataset.py
@@ -4,7 +4,6 @@
 # For dataset details visit: https://huggingface.co/datasets/samsum
 
 import copy
-import datasets
 import itertools
 from datasets import load_dataset
 
@@ -65,62 +64,4 @@ def get_custom_dataset(dataset_config, tokenizer, split):
     # dataset = dataset.filter(lambda example: len(example["input_ids"]) <= 8192)
     dataset = dataset.filter(lambda example: len(example["input_ids"]) <= 32768)
     print(f"After filtering: {len(dataset)}")
-    return dataset
-
-def get_custom_dataset_example(dataset_config, tokenizer, split):
-    dataset = datasets.load_dataset("OpenAssistant/oasst1", split=split)
-
-    dataset = dataset.map(lambda sample: {
-        "message_id": sample["message_id"],
-        "parent_id": sample["parent_id"],
-        "text": sample["text"],
-        },
-        batched=True,
-        remove_columns=list(dataset.features),)
-
-    nodes = {}
-
-    messages = {}
-    root_ids = []
-
-    for data in dataset:
-        if data["parent_id"]:
-            nodes[data["parent_id"]] = nodes.get(data["parent_id"], []) + [data["message_id"]]
-        else:
-            root_ids.append(data["message_id"])
-        messages[data["message_id"]]=data["text"]
-
-    def follow(thread, current_id):
-        thread = copy.copy(thread) + [messages[current_id]]
-        if current_id in nodes:
-            new_threads = []
-            for next_id in nodes[current_id]:
-                new_threads += follow(thread, next_id)
-            return new_threads
-        else:
-            return [thread]
-
-    def get_threads_from_root(root_id):
-        all_threads = []
-        thread = [messages[root_id]]
-        for cid in nodes[root_id]:
-            all_threads += follow(thread, cid)
-        return all_threads
-
-    dataset = dataset.filter(lambda x: x["message_id"] in root_ids)
-    dataset = dataset.map(lambda x: {"thread": get_threads_from_root(x["message_id"])}, remove_columns=list(dataset.features))
-    dataset = dataset.map(lambda x: {"thread": [i for row in x["thread"] for i in row]}, batched=True)
-
-    def to_dialog(thread):
-        dialog = []
-        for i, content in enumerate(thread):
-            dialog.append({
-                "role": "user" if i % 2 == 0 else "assistant",
-                "content": content,
-            })
-        return {"dialog": dialog}
-
-    dataset = dataset.map(lambda x: to_dialog(x["thread"]), remove_columns=list(dataset.features))
-    dataset = dataset.map(lambda x: tokenize_dialog(x["dialog"], tokenizer), remove_columns=list(dataset.features))
-
     return dataset
